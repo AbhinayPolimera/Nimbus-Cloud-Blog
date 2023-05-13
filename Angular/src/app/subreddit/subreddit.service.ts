8936f56b579f7a90116c8aebd0f3c1bbb364abb2
Bhavyanth <bhavyanth02@gmail.com>
2023-05-12 21:19:47 -0500

    Uploading architecture diagram

--- Angular/src/app/subreddit/subreddit.service.ts
+++ Angular/src/app/subreddit/subreddit.service.ts
@@ -10,11 +10,11 @@ export class SubredditService {
   constructor(private http: HttpClient) { }
 
   getAllSubreddits(): Observable<Array<SubredditModel>> {
-    return this.http.get<Array<SubredditModel>>('http://localhost:8080/api/category');
+    return this.http.get<Array<SubredditModel>>('http://localhost:8081/api/category');
   }
 
   createSubreddit(subredditModel: SubredditModel): Observable<SubredditModel> {
-    return this.http.post<SubredditModel>('http://localhost:8080/api/category',
+    return this.http.post<SubredditModel>('http://localhost:8081/api/category',
       subredditModel);
   }
 }
